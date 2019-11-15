f7f5e264341526638e3e89e7de9b8e9e5bb92afc
Aanuragkumarsrivastava <42467453+Aanuragkumarsrivastava@users.noreply.github.com>
2019-11-15 12:53:17 +0530

    added program on exception handling
    here used thread.sleep method

---
+++ Exceptiondemo2.java
@@ -0,0 +1,19 @@
+package pack3;
+
+public class Exceptiondemo2 
+{
+	//using throw
+
+	public static void main(String[] args) throws Exception
+	{
+		// TODO Auto-generated method stub
+		
+		for(int i=0;i<=20;i+=2)
+		{
+			Thread.sleep(1000);
+			System.out.println(i);
+		}
+
+	}
+
+}
